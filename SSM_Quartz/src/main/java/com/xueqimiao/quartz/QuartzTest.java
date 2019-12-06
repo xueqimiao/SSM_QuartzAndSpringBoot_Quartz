0398b82d15ece21378d0d4220dcf0cebf42f562f
yeasin <yeasin@qq.com>
2019-12-06 16:39:25 +0800

    xueqimiao

---
+++ SSM_Quartz/src/main/java/com/xueqimiao/quartz/QuartzTest.java
@@ -0,0 +1,10 @@
+package com.xueqimiao.quartz;
+
+import java.util.Date;
+
+public class QuartzTest {
+
+	private void configureTasks() {
+        System.out.println("执行静态定时任务时间:" + new Date());
+    }
+}
